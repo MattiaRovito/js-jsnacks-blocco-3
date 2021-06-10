61c7eeb87886bb467577b4101525493dda259ec5
MattiaR <rovitomattia91@gmail.com>
2021-06-10 14:32:50 +0200

    Add 1 esercizio

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
 
 
 
-// Todo Esercizio 2. Dare la possibilità di inserire due parole.      Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+// Todo Esercizio 2. Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
 
 
@@ -28,7 +28,33 @@
 // Todo Esercizio 3. Scrivi una funzione che fonda due array (con lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altroes. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
 
+var primoArray = ['a','b','c','d','e'];
+var secondoArray = [1,2,3,4,5];
 
+function unisci(array1, array2){
+
+    var arrayCumulativo = [];
+    
+    // *Mettiamo un controllo: SE la lunghezza dei due array da cumulare è uguale, allora fai il FOR, altrimenti alert.
+    
+    if (array1.length == array2.length){
+
+        // *In questo caso mettere i < array1.length o array2.length è la stessa cosa, perché richiediamo la stessa lunghezza.
+
+        for(var i = 0; i < array1.length; i++){
+            arrayCumulativo.push(array1[i], array2[i]);
+        }
+        return arrayCumulativo;
+    } else {
+        alert('La lunghezza dei due array non è uguale. Riprovare')
+    }
+   
+}
+
+// *Creiamo una variabile per far 'attivare' la funzione, quindi facciamo il console.log della variabile (o i vari document.getElementById...)
+
+var terzoArray = unisci(primoArray, secondoArray);
+console.log (terzoArray);
 
 
 

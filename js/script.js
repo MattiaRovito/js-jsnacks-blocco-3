624fad8e1fa206d7992e47e724f3daf7f27f7a36
MattiaR <rovitomattia91@gmail.com>
2021-06-10 14:22:06 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Todo Esercizio 1. Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).  La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+
+
+
+
+
+
+
+
+
+
+
+// Todo Esercizio 2. Dare la possibilità di inserire due parole.      Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Todo Esercizio 3. Scrivi una funzione che fonda due array (con lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altroes. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+
+
+
+
+
+
+
+
+
+// Todo Esercizio 4. Scrivi una funzione che prenda una stringa e la trasformi con l'iniziale maiuscola e tutto il resto in minuscolo.

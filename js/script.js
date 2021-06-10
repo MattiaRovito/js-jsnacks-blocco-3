61f67dfd721c4737d6f52df87ffeb775b1d3418c
MattiaR <rovitomattia91@gmail.com>
2021-06-10 14:59:06 +0200

    add 4 esercizio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
 // Todo Esercizio 1. Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).  La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
 
+// *input
+
+var arrayIniziale = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var a = parseInt(prompt('Inserisci un numero minimo 1 e un numero massimo ' + arrayIniziale.length));
+var b = parseInt(prompt('Inserisci un numero massimo ' + arrayIniziale.length));
+
+
+// *funzione
+
+function rangeArray(array, min, max)
+{
+    var newArray =[];
+    for (var i = min - 1; i < max; i++){
+        newArray.push(array[i]);
+    }
+    return newArray;
+}
+
+
+// *alert
+
+while (a > b || b > arrayIniziale.length){
+    alert('Attenzione: inserire correttamente i valori');
+    var a = parseInt(prompt('Inserisci un numero minimo 1 massimo'  + arrayIniziale.length));
+    var b = parseInt(prompt('Inserisci un numero massimo' + arrayIniziale.length));
+}
+
+
+// *output
+
+console.log(rangeArray(arrayIniziale, a, b));
+
+
+
+
+
+
 
 
 
